8b8450a8b664b03bc1f9fc09b6551d8450008b6e
Zahary Karadjov <zahary@metacraft-labs.com>
2026-06-20 08:34:57 +0300

    ci: point trace-format nim-stew path at src

--- repro.nim
+++ repro.nim
@@ -112,9 +112,9 @@ package codetracer_cairo_recorder:
       "target/release/codetracer-cairo-recorder" & binarySuffix
     const traceFormatNimExtraPaths =
       "../../codetracer-cairo-recorder/.reprobuild-src/libs/results/src:" &
-      "../../codetracer-cairo-recorder/.reprobuild-src/libs/nim-stew:" &
+      "../../codetracer-cairo-recorder/.reprobuild-src/libs/nim-stew/src:" &
       "../../codetracer-cairo-recorder/reprobuild/libs/results/src:" &
-      "../../codetracer-cairo-recorder/reprobuild/libs/nim-stew"
+      "../../codetracer-cairo-recorder/reprobuild/libs/nim-stew/src"
     let traceFormatNimEnv = @[
       ("CODETRACER_TRACE_FORMAT_NIM_SKIP_NIMBLE_INSTALL", "1"),
       ("CODETRACER_TRACE_FORMAT_NIM_EXTRA_PATHS", traceFormatNimExtraPaths)
