674fd513f37199e1d144b4bfe027ab93fd17958b
Zahary Karadjov <zahary@metacraft-labs.com>
2026-07-07 01:08:44 +0300

    ci: use vendored Nim deps for ct-print

--- repro.nim
+++ repro.nim
@@ -227,10 +227,19 @@ package codetracer_cairo_recorder:
     let ctPrintBuild = shell(
       command =
         "set -euo pipefail; " &
+        "recorder_root=\"$PWD\"; " &
         "cd ../codetracer-trace-format-nim; " &
-        "nimble install -y stew results; " &
-        "nim c -d:release --mm:arc -p:src -o:ct-print " &
-          "src/codetracer_ct_print.nim; " &
+        "if [ -d \"$recorder_root/.reprobuild-src/libs/results/src\" ] && " &
+            "[ -d \"$recorder_root/.reprobuild-src/libs/nim-stew/src\" ]; then " &
+          "nim c -d:release --mm:arc -p:src " &
+            "-p:\"$recorder_root/.reprobuild-src/libs/results/src\" " &
+            "-p:\"$recorder_root/.reprobuild-src/libs/nim-stew/src\" " &
+            "-o:ct-print src/codetracer_ct_print.nim; " &
+        "else " &
+          "nimble install -y stew results; " &
+          "nim c -d:release --mm:arc -p:src -o:ct-print " &
+            "src/codetracer_ct_print.nim; " &
+        "fi; " &
         "test -f ct-print" & binarySuffix,
       actionId = "codetracer-cairo-recorder.ct-print-build",
       cacheable = false)
